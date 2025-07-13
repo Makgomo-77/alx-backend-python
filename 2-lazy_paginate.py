b7735ff03b4b51eae8e41544178896dd1b4691e5
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-07-13 17:48:07 +0200

    Update 2-lazy_paginate.py

--- 2-lazy_paginate.py
+++ 2-lazy_paginate.py
@@ -1,10 +1,10 @@
 def paginate_users(page_size,offeset):
-  all_users= [f"User_{i} for i in range(1,101)"]
+  all_users= [f"User_{i}" "for i in range(1,101)"]
   start = offset
   end = offset + page_size
   return all_users[start:end]
 
-def lazy_paginate(page_size)
+def lazy_paginate(page_size):
 offset=0
 current_page =[]
 index= 0
@@ -12,11 +12,11 @@ index= 0
 while True:
   if index >= len(current_page):
     current_page= paginate_users(page_size,offset)
-    if not current_page
+    if not current_page:
     break
     offset +=page_size
-    index=0
+    index=0    
 
-if index < len(current_page)
+if index < len(current_page):
 yield current_page[index]
 index+= 1
